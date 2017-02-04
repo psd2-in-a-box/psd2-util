64edbb7cdc28e32bdf9af0b3e088855cd62740c5
Morten Hindsholm <morten@hindsholm.dk>
2017-02-04 22:23:38 +0100

    Initial attempt

---
+++ README.md
@@ -0,0 +1,3 @@
+# psd2-util
+
+Utilities for PSD2-in-a-box
---
+++ transaction_pump.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""The Transaction Pump"""
+
+import json
+import time
+import random
+import requests
+
+USER = 'user099'
+PASSWORD = 'TSuNHAWuHYwH'
+
+URL = 'http://api.futurefinance.io/api'
+FROM_ACCOUNT = '4574000007'
+TO_ACCOUNT = '4574000008'
+
+TXTS = ['Bilka, Skalborg',
+        'Kvickly, Randers',
+        'H&M, Lyngby',
+        'Amazon.com',
+        'SuperBrugsen, Hadsund',
+        'Gistrup Bageri',
+        'Netflix']
+
+HEADERS = {'Content-Type': 'application/json'}
+
+
+while True:
+    PAYLOAD = {'destinationAccountNumber': TO_ACCOUNT,
+               'amount': 1000 * random.random(),
+               'postingText': random.choice(TXTS)}
+
+    R = requests.post(URL + '/accounts/' + FROM_ACCOUNT + '/transactions',
+                      headers=HEADERS,
+                      auth=(USER, PASSWORD),
+                      data=json.dumps(PAYLOAD))
+    print('Response:', json.dumps(R.json(), sort_keys=True, indent=4))
+
+    time.sleep(5)
